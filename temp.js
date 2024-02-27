32da693b02f547bbee41b051c79c345fddf45a5f
huyhoang <crphuong2005@gmail.com>
2024-02-27 16:45:29 +0700

    [DRATF] [display and details]

---
+++ temp.js
@@ -0,0 +1,63 @@
+var films = [
+  {
+    imageUrl: "https://images.metahub.space/poster/small/tt15398776/img",
+    name: "Oppenheimer",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+  {
+    imageUrl:
+      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
+    name: "test",
+  },
+];
+
+localStorage.setItem("films", JSON.stringify(films));
