275fb3db231d74abd2826600e0d3c500d06d0b45
huyhoang <crphuong2005@gmail.com>
2024-02-28 10:11:47 +0700

    [wf-1/004,tìm kiếm phim][wf-1/006,đề xuất phim]

--- temp.js
+++ temp.js
@@ -5,48 +5,48 @@ var films = [
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt26047818/img",
+    name: "Anyone But You",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt7160372/img",
+    name: "The Zone of Interest",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt1160419/img",
+    name: "Dune",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt8521778/img",
+    name: "Bob Marley: One Love",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt19864802/img",
+    name: "Land of Bad",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt17351924/img",
+    name: "Saltburn",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt21064584/img",
+    name: "The Iron Claw",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt1563738/img",
+    name: "One Day",
   },
   {
     imageUrl:
-      "https://i.pinimg.com/originals/ef/af/ed/efafed5f85f7e33434dc0712022fc8d7.jpg",
-    name: "test",
+      "https://images.metahub.space/poster/small/tt14849194/img",
+    name: "The Holdovers",
   },
   {
     imageUrl:
@@ -61,3 +61,22 @@ var films = [
 ];
 
 localStorage.setItem("films", JSON.stringify(films));
+
+imgShow = [
+  {
+    nameImg:"https://images.metahub.space/logo/medium/tt15398776/img",
+    imageUrl:"https://images.metahub.space/background/medium/tt15398776/img",
+  },
+  { 
+    nameImg:"https://images.metahub.space/logo/medium/tt26047818/img",
+    imageUrl:"https://images.metahub.space/background/medium/tt26047818/img",
+  },
+  {
+    nameImg:"https://images.metahub.space/logo/medium/tt7160372/img",
+    imageUrl:"https://images.metahub.space/background/medium/tt7160372/img",
+  }
+];
+
+localStorage.setItem("imgShow",JSON.stringify(imgShow));
+
+
